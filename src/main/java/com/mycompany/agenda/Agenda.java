558f05fb5b78e21baa884786b36b98f960b0a6e0
chuan5614515 <chinoputoamo20031031@gmail.com>
2023-02-23 23:42:53 +0100

    Primer commit

---
+++ src/main/java/com/mycompany/agenda/Agenda.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenda;
+
+/**
+ *
+ * @author chuan
+ */
+public class Agenda {
+
+    public static void main(String[] args) {
+   
+	Inicio i = new Inicio();
+	i.setVisible(true);
+	
+	  
+    }
+}
